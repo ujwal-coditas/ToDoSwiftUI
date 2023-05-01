8d5a9ce53623a76ba96898d526058eefaace0ae4
Ujwal Thote <ujwal.thote@coditas.com>
2023-05-01 20:36:40 +0530

    Initial commit

---
+++ Todo/Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by uthote-lap-0266 on 01/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
